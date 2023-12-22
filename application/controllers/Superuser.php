d6a5cab7d81ca626824a8dcf5506afc449cea203
gilangdwic <gilangdwc865@gmail.com>
2023-12-22 15:36:21 +0700

    pertemuan 1-12

---
+++ application/controllers/Superuser.php
@@ -0,0 +1,9 @@
+<?php
+//edit
+class Superuser extends CI_Controller
+{
+    public function index()
+    {
+        redirect('autentifikasi');
+    }
+}
---
+++ application/views/v_footer.php
@@ -0,0 +1,7 @@
+		<footer>
+			<a href="http://www.RentalBuku.com">RentalBuku</a>
+	//P3 menambahkan v-about.php, v-footer.php, v-header.php
+	    </footer>
+	</div>
+</body>
+</html>
